7320cd9d4d31aaee4943931344f28b172de943a6
Zakaria <zaknetsum@gmail.com>
2023-10-26 04:13:17 +0600

    chnage api url

--- app/(admin)/deposit/[depositId]/page.tsx
+++ app/(admin)/deposit/[depositId]/page.tsx
@@ -6,6 +6,15 @@ import {
 } from '@/redux/features/deposit/depositApi';
 import { fetchBaseQueryError } from '@/redux/helpers';
 import { toast } from 'react-toastify';
+import {
+	Button,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogContentText,
+	DialogTitle,
+	Modal,
+} from '@mui/material';
 
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
@@ -35,22 +44,21 @@ const Deposit = ({ params }: { params: { depositId: string } }) => {
 	} = deposit || {};
 
 	const [reason, setReason] = useState('Transaction Id not matching');
-	const [show, setShow] = useState(false);
-	const [showReject, setShowReject] = useState(false);
-	const [showApprove, setShowApprove] = useState(false);
+	const [open, setOpen] = React.useState(false);
+	const [open2, setOpen2] = React.useState(false);
+
+	const handleClickOpen = () => {
+		setOpen(true);
+	};
+	const handleClickOpen2 = () => {
+		setOpen2(true);
+	};
 
 	const handleClose = () => {
-		setShow(false);
-		setShowApprove(false);
-		setShowReject(false);
+		setOpen(false);
 	};
-	const handleShow = (item: any) => {
-		setShow(true);
-		if (item === 'approve') {
-			setShowApprove(true);
-		} else if (item === 'reject') {
-			setShowReject(true);
-		}
+	const handleClose2 = () => {
+		setOpen2(false);
 	};
 
 	const [
@@ -80,7 +88,6 @@ const Deposit = ({ params }: { params: { depositId: string } }) => {
 
 	useEffect(() => {
 		if (a_isSuccess) {
-			setShow(false);
 			toast.success('Deposit approved successfully');
 			router.push('/deposit');
 		}
@@ -101,7 +108,6 @@ const Deposit = ({ params }: { params: { depositId: string } }) => {
 
 	useEffect(() => {
 		if (r_isSuccess) {
-			setShow(false);
 			toast.success('Deposit rejected successfully');
 			router.push('/deposit');
 		}
@@ -113,62 +119,6 @@ const Deposit = ({ params }: { params: { depositId: string } }) => {
 
 	return (
 		<div>
-			{/* <!-- Alerts Item --> */}
-			{show && (
-				<div className='my-4 '>
-					<div className='flex w-full border-l-6 border-warning bg-warning bg-opacity-[15%] px-7 py-4 shadow-md dark:bg-[#1b1b24c4] dark:bg-opacity-30 md:p-9'>
-						<div className='flex items-center justify-center mr-5 rounded-lg h-9 w-9 bg-warning bg-opacity-30'>
-							<svg
-								width='19'
-								height='16'
-								viewBox='0 0 19 16'
-								fill='none'
-								xmlns='http://www.w3.org/2000/svg'
-							>
-								<path
-									d='M1.50493 16H17.5023C18.6204 16 19.3413 14.9018 18.8354 13.9735L10.8367 0.770573C10.2852 -0.256858 8.70677 -0.256858 8.15528 0.770573L0.156617 13.9735C-0.334072 14.8998 0.386764 16 1.50493 16ZM10.7585 12.9298C10.7585 13.6155 10.2223 14.1433 9.45583 14.1433C8.6894 14.1433 8.15311 13.6155 8.15311 12.9298V12.9015C8.15311 12.2159 8.6894 11.688 9.45583 11.688C10.2223 11.688 10.7585 12.2159 10.7585 12.9015V12.9298ZM8.75236 4.01062H10.2548C10.6674 4.01062 10.9127 4.33826 10.8671 4.75288L10.2071 10.1186C10.1615 10.5049 9.88572 10.7455 9.50142 10.7455C9.11929 10.7455 8.84138 10.5028 8.79579 10.1186L8.13574 4.75288C8.09449 4.33826 8.33984 4.01062 8.75236 4.01062Z'
-									fill='#FBBF24'
-								></path>
-							</svg>
-						</div>
-						<div className='w-full space-y-4'>
-							<div>
-								<h5 className='mb-3 text-lg font-semibold text-[#9D5425]'>
-									Attention needed
-								</h5>
-								<p className='leading-relaxed text-[#D0915C]'>
-									Are you sure you want to approve or reject this deposit?
-								</p>
-							</div>
-							<div className='flex items-center justify-around '>
-								{showApprove && (
-									<button
-										onClick={handleApprove}
-										className='inline-flex items-center justify-center px-10 py-2 font-medium text-center text-white rounded-md bg-primary hover:bg-opacity-90 lg:px-8 xl:px-10'
-									>
-										<span>Approve</span>
-									</button>
-								)}
-								{showReject && (
-									<button
-										onClick={handleReject}
-										className='px-4 py-2 text-white bg-red-500 rounded-md hover:bg-red-600'
-									>
-										<span>Reject</span>
-									</button>
-								)}
-								<button
-									onClick={handleClose}
-									className='px-4 py-2 text-white bg-red-500 rounded-md hover:bg-red-600'
-								>
-									<span>Cancel</span>
-								</button>{' '}
-							</div>
-						</div>
-					</div>
-				</div>
-			)}
-			{/* <!-- Alerts Item --> */}
 			<div className='px-4 py-1 list-none border text-blue-gray-400 '>
 				{/* Amount */}
 				<div className='grid grid-cols-2'>
@@ -246,19 +196,89 @@ const Deposit = ({ params }: { params: { depositId: string } }) => {
 			{status === 'pending' && (
 				<div className='grid gap-2 mt-2'>
 					<button
-						onClick={() => handleShow('approve')}
+						onClick={handleClickOpen}
 						className='inline-flex items-center justify-center px-10 py-2 font-medium text-center text-white rounded-md bg-primary hover:bg-opacity-90 lg:px-8 xl:px-10'
 					>
 						<span>Approve</span>
 					</button>{' '}
 					<button
-						onClick={() => handleShow('reject')}
+						onClick={handleClickOpen2}
 						className='px-4 py-2 text-white bg-red-500 rounded-md hover:bg-red-600'
 					>
 						<span>Reject</span>
 					</button>{' '}
 				</div>
 			)}
+			<>
+				<div>
+					<Dialog
+						open={open}
+						onClose={handleClose}
+						aria-labelledby='alert-dialog-title'
+						aria-describedby='alert-dialog-description'
+					>
+						<DialogTitle id='alert-dialog-title'>
+							{'Approve Deposit'}
+						</DialogTitle>
+						<DialogContent>
+							<DialogContentText id='alert-dialog-description'>
+								<div>
+									<p className=' text-warning'>
+										Are you sure you want to approve this Deposit?
+									</p>
+								</div>
+							</DialogContentText>
+						</DialogContent>
+						<DialogActions>
+							<Button onClick={handleClose}>Close</Button>
+							<Button onClick={handleApprove}>Approve</Button>
+						</DialogActions>
+					</Dialog>
+				</div>
+			</>
+			{/* for reject */}
+			<>
+				<div>
+					<Dialog
+						open={open2}
+						onClose={handleClose2}
+						aria-labelledby='alert-dialog-title'
+						aria-describedby='alert-dialog-description'
+					>
+						<DialogTitle id='alert-dialog-title'>
+							{'Reject Deposit'}
+						</DialogTitle>
+						<DialogContent>
+							<DialogContentText id='alert-dialog-description'>
+								<div>
+									<p className=' text-warning'>
+										Are you sure you want to Reject this Deposit?
+									</p>
+									<div className=' flex flex-col'>
+										<label htmlFor='basic-url'>
+											<span>Enter Reason</span>
+										</label>
+										<input
+											className='w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary text-white'
+											placeholder='Enter Reason'
+											aria-label='Username'
+											aria-describedby='basic-addon1'
+											value={reason}
+											onChange={(e) => setReason(e.target.value)}
+										/>
+									</div>
+								</div>
+							</DialogContentText>
+						</DialogContent>
+						<DialogActions>
+							<Button onClick={handleClose2}>Close</Button>
+							<Button onClick={handleReject} disabled={reason === ''}>
+								Reject
+							</Button>
+						</DialogActions>
+					</Dialog>
+				</div>
+			</>
 		</div>
 	);
 };
