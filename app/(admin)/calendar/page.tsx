f352667a6bd52e72892e9ffce4a08f4db1ef2206
Zakaria <zaknetsum@gmail.com>
2023-12-06 03:33:42 +0600

    testing 01

--- app/(admin)/calendar/page.tsx
+++ app/(admin)/calendar/page.tsx
@@ -1,18 +1,18 @@
-import Calendar from "@/components/Calender";
-import { Metadata } from "next";
+import Calendar from '@/components/Calender';
+import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "Calendar Page | Next.js E-commerce Dashboard Template",
-  description: "This is Calendar page for TailAdmin Next.js",
-  // other metadata
+	title: 'Calendar Page | Next.js E-commerce Dashboard Template',
+	description: 'This is Calendar',
+	// other metadata
 };
 
 const CalendarPage = () => {
-  return (
-    <>
-      <Calendar />
-    </>
-  );
+	return (
+		<>
+			<Calendar />
+		</>
+	);
 };
 
 export default CalendarPage;
